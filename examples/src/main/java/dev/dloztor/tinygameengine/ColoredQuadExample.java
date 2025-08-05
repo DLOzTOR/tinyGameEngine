8a30a445ac59b6b934c3d664f3adcd2581481ec2
DLOzTOR <dloztor.aar@outlook.com>
2025-08-05 13:16:03 +0300

    create ColoredQuadExample class but shaders isnt load

--- examples/src/main/java/dev/dloztor/tinygameengine/ColoredQuadExample.java
+++ examples/src/main/java/dev/dloztor/tinygameengine/ColoredQuadExample.java
@@ -1,19 +1,26 @@
 package dev.dloztor.tinygameengine;
 
 import dev.dloztor.tinygameengine.core.Window;
+import dev.dloztor.tinygameengine.graphics.primitives.ColoredQuad;
+import org.joml.Vector3f;
 
 public class ColoredQuadExample {
     private static class MyWindow extends Window {
+        ColoredQuad cq;
         public MyWindow(int width, int height, String title) {
             super(width, height, title);
         }
         @Override
-        protected void loop(){
-
+        protected void onInit(){
+            cq = new ColoredQuad(new Vector3f(1, 1, 1));
+        }
+        @Override
+        protected void onUpdateFrame(){
+            cq.draw();
         }
     }
 
     public static void main(String[] args) {
-
+        new MyWindow(400, 400, "Colored quad showcase").run();
     }
 }
